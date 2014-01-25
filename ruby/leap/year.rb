578901428a1bd077fa1bde77d678fe4467360dd8
Jeff Casimir <jeff@casimircreative.com>
2014-01-24 20:50:15 -0600

    Starting to extract a LeapYear class

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,5 +1,22 @@
 class Year
   def self.leap?(input)
-    (input % 400 == 0) || (input.even? && !(input % 100 == 0))
+    year = LeapYear.new(input)
+    year.valid? || (input.even? && !(input % 100 == 0))
+  end
+end
+
+class LeapYear
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+
+  def valid?
+    four_hundred?
+  end
+
+  def four_hundred?
+    year % 400 == 0
   end
 end
