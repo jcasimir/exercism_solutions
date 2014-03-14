e76f2a1eef16d08b38c6ef956bac8d5a3c3c472f
Jeff Casimir <jeff@casimircreative.com>
2014-03-13 21:37:47 -0600

    Way too much work to remove non-letters

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,15 +1,17 @@
 var words = function(input){
   'use strict';
-
   var individual_words = input.split(' ');
   var output = {};
 
   for(var position in individual_words){
-    var word = individual_words[position]
-    if(output[word]){
-      output[word] = output[word] + 1;
-    } else {
-      output[word] = 1;
+    var word = individual_words[position];
+    word = word.replace(/\W/g, '');
+    if(word.match(/\w/)){
+      if(output[word]){
+        output[word] = output[word] + 1;
+      } else {
+        output[word] = 1;
+      }
     }
   }
   return output;
--- javascript/word-count/word-count_test.spec.js
+++ javascript/word-count/word-count_test.spec.js
@@ -11,12 +11,12 @@ describe("words()", function() {
     expect(words("one of each")).toEqual(expectedCounts);
   });
 
-  xit("counts multiple occurrences", function() {
+  it("counts multiple occurrences", function() {
     var expectedCounts = { one: 1, fish: 4, two: 1, red: 1, blue: 1 };
     expect(words("one fish two fish red fish blue fish")).toEqual(expectedCounts);
   });
 
-  xit("ignores punctuation", function() {
+  it("ignores punctuation", function() {
     var expectedCounts = { car: 1, carpet: 1, as: 1, java: 1, javascript: 1 };
     expect(words("car : carpet as java : javascript!!&@$%^&")).toEqual(expectedCounts);
   });
