1ca3020bb7be19625ec30d2a46d988543dd3dd91
Jeff Casimir <jeff@casimircreative.com>
2014-03-17 21:29:12 -0600

    First implementation of anagram finding

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,9 +1,20 @@
 var Anagram = function(word){
   this.word = word;
 
-  this.match = function(){
-    return [];
+  this.match = function(possiblities){
+    var results = possiblities.filter(function (possibility){
+      // console.log("Looking at " + possibility);
+      // console.log("  - " + sortedLetters(possibility));
+      // console.log("  - " + sortedLetters(word));
+      // console.log("  = " + sortedLetters(possibility) === sortedLetters(word));
+      return sortedLetters(possibility) === sortedLetters(word);
+    });
+    return results;
   };
+
+  sortedLetters = function(input){
+    return input.split('').sort().join().toLowerCase();
+  }
 };
 
 module.exports = Anagram;
