383a1798e7a731169469a70ac7a182554bca4ae3
Jeff Casimir <jeff@casimircreative.com>
2014-03-19 21:28:16 -0600

    First structure for beer-song

---
+++ javascript/beer-song/beer-song.js
@@ -0,0 +1,7 @@
+var BeerSong = {
+  verse : function(){
+    return "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
+  }
+};
+
+module.exports = BeerSong;
