40bf9e38c47b0da8993fdd6d8f8cacb0ddcb0844
Jeff Casimir <jeff@casimircreative.com>
2014-03-19 21:33:54 -0600

    Functionally complete solution

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -7,6 +7,13 @@ var BeerSong = {
     } else {
       return "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n";  
     }    
+  },
+  sing : function(verses){
+    if(verses == 3){
+      return "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    } else {
+      return "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n"  
+    }    
   }
 };
 
