b10a4c03c92b8dc49be58af45abac5d552dd84d5
Jeff Casimir <jeff@casimircreative.com>
2014-03-20 21:06:19 -0600

    Added more dynamic string calculations rather than static strings

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,18 +1,22 @@
 var BeerSong = {
   verse : function(count){
-    if(count == 1){
-      return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
-    } else if(count == 0){
+    if(count == 0){
       return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    } else if(count == 1){
+      return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
     } else {
-      return "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n";  
+      return count + " bottles of beer on the wall, " + count + " bottles of beer.\nTake one down and pass it around, " + (count-1) + " bottles of beer on the wall.\n";  
     }    
   },
-  sing : function(verses){
-    if(verses == 3){
-      return "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+  sing : function(verses_start, verses_stop){
+    var result = "";
+    if(verses_stop){
+      for(var v = verses_start; v > verses_stop; v--){
+        result = result + this.verse(v) + '\n';
+      }
+      return result + this.verse(v)      
     } else {
-      return "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n"  
+      return "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"  
     }    
   }
 };
