67100b229f84a37bba2adb97fd569e9e42407049
Jeff Casimir <jeff@casimircreative.com>
2014-03-21 21:42:10 -0600

    Extracted pluraliation out to functions

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -2,12 +2,8 @@ var BeerSong = {
   verse : function(count){
     if(count == 0){
       return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    } else if(count == 2){
-      return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n";
-    } else if(count == 1){
-      return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
     } else {
-      return count + " bottles of beer on the wall, " + count + " bottles of beer.\nTake one down and pass it around, " + (count-1) + " bottles of beer on the wall.\n";  
+      return this.bottles(count) + " of beer on the wall, " + this.bottles(count) + " of beer.\nTake " + this.next_bottle(count) + " down and pass it around, " + this.bottles(count - 1) + " of beer on the wall.\n";  
     }    
   },
   sing : function(verses_start, verses_stop){
@@ -17,7 +13,23 @@ var BeerSong = {
       result = result + this.verse(v) + '\n';
     }
     return result + this.verse(v);
-  }  
+  },
+  bottles : function(count){
+    if(count == 0){
+      return "no more bottles"
+    } else if(count == 1){
+      return "1 bottle";
+    } else {
+      return count + " bottles";
+    }
+  },
+  next_bottle : function(count){
+    if(count == 1){
+      return "it";
+    } else {
+      return "one";
+    }
+  }
 };
 
 module.exports = BeerSong;
