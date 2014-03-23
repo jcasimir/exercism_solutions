de597545df332130185d6901568b7450aa408edd
Jeff Casimir <jeff@casimircreative.com>
2014-03-22 21:22:25 -0600

    Finished simplistic counting of a single marker

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,6 +1,15 @@
-var DNA = function(input){
+var DNA = function(sequence){
+
   return {
-    nucleotideCounts : { 'A' : 0, 'T' : 0, 'C' : 0, 'G' : 0 }
+    nucleotideCounts : { 
+      'A' : 0, 
+      'T' : 0, 
+      'C' : 0, 
+      'G' : 0 
+    },
+    count : function(marker){ 
+      return sequence.split(marker).length - 1;
+    }
   };
 };
 
--- javascript/nucleotide-count/nucleotide-count_test.spec.js
+++ javascript/nucleotide-count/nucleotide-count_test.spec.js
@@ -8,12 +8,12 @@ describe('DNA', function() {
     expect(dna.nucleotideCounts).toEqual(expected);
   });
 
-  xit('has no adenosine', function(){
+  it('has no adenosine', function(){
     var dna = new DNA('');
     expect(dna.count('A')).toEqual(0);
   });
 
-  xit('repetitive cytidine gets counts', function(){
+  it('repetitive cytidine gets counts', function(){
     var dna = new DNA('CCCCC');
     expect(dna.count('C')).toEqual(5);
   });
