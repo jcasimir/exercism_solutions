2a9a2b6dc49b1833e23d6442e749791f5d8d5f6b
Jeff Casimir <jeff@casimircreative.com>
2014-04-03 21:05:15 -0600

    Legitimate removal of non-digits from the input

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,7 +1,13 @@
 var PhoneNumber = function(input){
   return {
     number : function(){
-      if(input.length == 11 && input[0] != '1'){
+      input = input.replace(/\D/gi, '');
+
+      if(input.length == 11 && input[0] == '1'){
+        input = input.slice(1, input.length);
+      }
+
+      if(input.length != 10){
         return '0000000000';
       } else {
         return '1234567890';
