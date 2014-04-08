2717ccec5fea786e983e37017e8af2a21264150e
Jeff Casimir <jeff@casimircreative.com>
2014-04-07 21:16:19 -0600

    Implement fetch to simplify grade-level access

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -1,13 +1,19 @@
 function School(){
   var storedNames = {};
+  this.fetch = function(level){
+    storedNames[level] = storedNames[level] || [];
+    return storedNames[level];
+  }
 
   this.roster = function(){
     return storedNames;
   };
-  this.add = function(name, grade){
-    storedNames[grade] = storedNames[grade] || [];
-    storedNames[grade].push(name);
-  }
+  this.add = function(name, level){
+    this.fetch(level).push(name);
+  };
+  this.grade = function(level){
+    return this.fetch(level).sort();
+  };
 };
 
 module.exports = School;
