5cd39204e206cefdb81c04f9d1b3a6f904dc21ca
Jeff Casimir <jeff@casimircreative.com>
2014-04-13 21:18:09 -0600

    Extracted component functions

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,5 +1,17 @@
 function Leap(year){
-  return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
+  function fourHundred(){
+    return year % 400 == 0;
+  };
+
+  function notOneHundred(){
+    return year % 100 != 0;
+  };
+
+  function four(){
+    return year % 4 == 0;
+  };
+
+  return fourHundred() || (notOneHundred() && four());
 };
 
 module.exports = Leap;
