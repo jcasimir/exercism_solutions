a7b75180d45732fc41f8c413588dccbe39978d94
Jeff Casimir <jeff@casimircreative.com>
2014-04-19 22:25:26 -0600

    Actually functional solution for basic counting

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,5 +1,13 @@
-function etl(){
-  return { a: 1 };
+function etl(inputs){
+  var results = {};
+  for (var key in inputs){
+    inputs[key].map(function(value){
+      value = value.toLowerCase();
+      results[value] = results[value] || 0;
+      results[value] += 1;
+    });
+  }
+  return results;
 };
 
 module.exports = etl;
