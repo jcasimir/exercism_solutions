926d7d5547e44e3a1d9ed803648864034fe1c01c
Jeff Casimir <jeff@casimircreative.com>
2014-04-27 18:24:33 -0500

    Functionally complete solution

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,7 +1,15 @@
 function Grains(){
-  this.square = function (){
-    return 1;
+  this.square = function(location){
+    return Math.pow(2,location - 1);
   };
+
+  this.total = function(){
+    var sum = 0;
+    for(var i = 1; i <= 64; i++){
+      sum += this.square(i);
+    }
+    return sum;
+  }
 };
 
 module.exports = Grains;
