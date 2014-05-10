eb68d928938a5c061c85475759cce68fe38b9578
Jeff Casimir <jeff@casimircreative.com>
2014-05-09 23:15:54 -0600

    Reducing my over-usage of this.

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,16 +1,12 @@
 function Triangle(side1, side2, side3){
-  this.side1 = side1;
-  this.side2 = side2;
-  this.side3 = side3;
-
   this.hasSides = function(){
-    if(!(this.side1 + this.side2 + this.side3 > 0)){
+    if(!(side1 + side2 + side3 > 0)){
       throw "Invalid triangle dimensions";
     };
   };
 
   this.hasNegativeSide = function(){
-    return (this.side1 <= 0 || this.side2 <= 0 || this.side3 <= 0);
+    return (side1 <= 0 || side2 <= 0 || side3 <= 0);
   };
 
   this.violatesTriangleEquality = function(){
@@ -22,11 +18,11 @@ function Triangle(side1, side2, side3){
   }
 
   this.isEquilateral = function(){
-    return (this.side1 === this.side2 && this.side2 === this.side3) && 'equilateral';
+    return (side1 === side2 && side2 === side3) && 'equilateral';
   }
 
   this.isIsosceles = function(){
-    return (this.side1 === this.side2 || this.side2 === this.side3 || this.side1 == this.side3) && 'isosceles';
+    return (side1 === side2 || side2 === side3 || side1 == side3) && 'isosceles';
   }
 
   this.kind = function(){
